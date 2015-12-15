89dcf5ac54ffeda4e79f484fe70931d9fa60ba82
William Chin <william-chin@outlook.com>
2015-12-15 16:05:44 -0500

    trying to get user location

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,42 @@
 var app = angular.module('meanMapApp', ['geolocation'])
 
   var map;
+  var currentLatitude;
+  var currentLongitude;
 
-  window.initMap = function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-      // styles: [{"stylers": [
-      //   { saturation: -20 },
-      //   { lightness: -20 },
-      //   { gamma: 1.51 }
-      // ]}]
-    });
-  }
+
+window.initMap = function initMap() {
+console.log('init map')
+
+  var getLocation = function() {
+    var success = function(pos) {
+      currentLatitude = pos.coords.latitude;
+        console.log(currentLatitude);
+      currentLongitude = pos.coords.longitude;
+        console.log(currentLongitude);
+        }
+      navigator.geolocation.getCurrentPosition(success);
+  };
+  getLocation();
+//console.log(getLocation)
+
+//map options
+  var mapOptions = {
+    zoom: 15,
+    center: {lat: 40.7400361, lng: -73.9897932},
+    panControl: false,
+    panControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE,
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    },
+    scaleControl: false
+
+    };
+
+map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+}
